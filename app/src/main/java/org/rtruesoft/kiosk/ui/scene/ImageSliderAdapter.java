af1ca20da61ccf01840d3e8c05b61a0eefe40b99
chb <chb1828@naver.com>
2020-08-27 11:03:07 +0900

    변수 선언해서 사진 개수 판별

--- app/src/main/java/org/rtruesoft/kiosk/ui/scene/ImageSliderAdapter.java
+++ app/src/main/java/org/rtruesoft/kiosk/ui/scene/ImageSliderAdapter.java
@@ -21,6 +21,7 @@ public class ImageSliderAdapter extends RecyclerView.Adapter<ImageSliderAdapter.
 
     private Context context;
     private List<Bitmap> photos;
+    private final int showImageCount = 4;
 
     public ImageSliderAdapter(Context context,List<Bitmap> photos) {
         this.context = context;
@@ -41,24 +42,64 @@ public class ImageSliderAdapter extends RecyclerView.Adapter<ImageSliderAdapter.
 
     @Override
     public void onBindViewHolder(@NonNull ImageSliderViewHolder holder, int position) {
-        System.out.println("뷰홀더 실행됨");
-        int standard = position*5;
-        holder.img1.setImageBitmap(photos.get(standard));
-        holder.img2.setImageBitmap(photos.get(standard+1));
-        holder.img3.setImageBitmap(photos.get(standard+2));
-        holder.img4.setImageBitmap(photos.get(standard+3));
-        holder.img5.setImageBitmap(photos.get(standard+4));
+        //int standard = position*5;
+        int standard = position*showImageCount;
+        if(standard<photos.size()) {
+            for(int i=standard; i<standard+showImageCount; i++) {
+                switch (i%showImageCount) {
+                    case 0:
+                        holder.img1.setImageBitmap(photos.get(i));
+                        break;
+                    case 1:
+                        holder.img2.setImageBitmap(photos.get(i));
+                        break;
+                    case 2:
+                        holder.img3.setImageBitmap(photos.get(i));
+                        break;
+                    case 3:
+                        holder.img4.setImageBitmap(photos.get(i));
+                        break;
+                  /*  case 4:
+                        holder.img5.setImageBitmap(photos.get(i));
+                        break;*/
+                }
+            }
+        }else {
+            int min = photos.size()-standard;
+            for(int i=min; i>=0; i--) {
+                int mind = photos.size()-i;
+                switch (mind%showImageCount) {
+                    case 0:
+                        holder.img1.setImageBitmap(photos.get(i));
+                        break;
+                    case 1:
+                        holder.img2.setImageBitmap(photos.get(i));
+                        break;
+                    case 2:
+                        holder.img3.setImageBitmap(photos.get(i));
+                        break;
+/*                    case 3:
+                        holder.img4.setImageBitmap(photos.get(i));
+                        break;*/
+                }
+            }
+        }
+
+/*            holder.img1.setImageBitmap(photos.get(standard));
+            holder.img2.setImageBitmap(photos.get(standard+1));
+            holder.img3.setImageBitmap(photos.get(standard+2));
+            holder.img4.setImageBitmap(photos.get(standard+3));
+            holder.img5.setImageBitmap(photos.get(standard+4));*/
     }
 
     @Override
     public int getItemCount() {
         int count = 0;
-        if(photos.size()%5 ==0) {
-            count = photos.size()/5;
+        if(photos.size()%showImageCount ==0) {
+            count = photos.size()/showImageCount;
         }else {
-            count = photos.size()/5 + 1 ;
+            count = photos.size()/showImageCount + 1 ;
         }
-        System.out.println(count+"개수");
         return count;   // 들어온 개수 / 5 ;
     }
 
@@ -72,7 +113,7 @@ public class ImageSliderAdapter extends RecyclerView.Adapter<ImageSliderAdapter.
             img2 = itemView.findViewById(R.id.sliderImage2);
             img3 = itemView.findViewById(R.id.sliderImage3);
             img4 = itemView.findViewById(R.id.sliderImage4);
-            img5 = itemView.findViewById(R.id.sliderImage5);
+           // img5 = itemView.findViewById(R.id.sliderImage5);
         }
     }
 
